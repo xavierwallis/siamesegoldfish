cd294b2c32efa8d9d566255e53c60de0915625ae
Xavier Wallis <wallisxavier04@gmail.com>
2022-05-22 17:17:10 -0400

    added head and font fix and overflow?

--- components/global/layout.jsx
+++ components/global/layout.jsx
@@ -1,8 +1,15 @@
 import Navbar from "./navbar";
 import Footer from './footer'
+import Head from "next/head";
 export default function Layout ( { children } ) {
     return (
         <>
+            <Head>
+                <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/open-sauce-sans" type="text/css"/>
+                <title>
+                    SIAMESE GOLDFISH
+                </title>
+            </Head>
             <Navbar />
             { children }
             <Footer />
