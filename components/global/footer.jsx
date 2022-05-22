fa8bea26c87fd7362dc5628d51ae3a7402ee8226
Xavier Wallis <wallisxavier04@gmail.com>
2022-05-22 17:09:32 -0400

    created favicon and added footer

--- components/global/footer.jsx
+++ components/global/footer.jsx
@@ -1,5 +1,9 @@
 export default function Footer () {
     return (
-        <></>
+        <>
+            <h1 className={'text-white text-center border-t border-t-white font-sans pt-4 mt-96'}>
+                WallisXDevelopment (c) 2022, wallisxdevelopment@gmail.com
+            </h1>
+        </>
     );
 }
